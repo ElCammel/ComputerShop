f5b74f0070cd215989df722aef6dfa0978f00855
Greg <ririo2@hotmail.fr>
2019-06-26 14:32:09 +0200

    update of order

--- ShoppingService/Order.cs
+++ ShoppingService/Order.cs
@@ -7,11 +7,33 @@ namespace ShoppingService
     {
         public Accounting.Address shippingAddress { get; set; }
         public PaymentService.IPayment paymentMetthod { get; set; }
-
+        private OrderStatus state;
+        public List<IProduct> Items { get; }
 
         public void PlaceOrder()
         {
 
+        }
+
+        public Order(OrderStatus state)
+        {
+            this.state = state;
+            Items = new List<IProduct>();
+        }
+
+        public void AddItem(IProduct item)
+        {
+            state.AddItem(item);
+        }
+
+        public void RemoveItem(IProduct item)
+        {
+            state.RemoveItem(item);
+        }
+
+        public void NextStatus(OrderStatus state)
+        {
+            this.state = state;
         }
     }
 }
